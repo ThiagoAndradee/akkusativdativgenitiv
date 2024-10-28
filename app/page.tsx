fab49cf186975f2965361d4395a092f2e56c687c
Thi <thiago.andradecontato@hotmail.com>
2024-10-28 21:15:36 +0100

    correcting gtm properties

--- app/page.tsx
+++ app/page.tsx
@@ -10,15 +10,16 @@ export default function Page() {
         <meta name="description" content="Learn and practice German grammar cases with interactive exercises. Master nominative, accusative, dative, and genitive cases effectively." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="UTF-8" />
-        {/* Google Tag Manager */}
-        <Script id="gtm-head" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-P49RJBX');`}
-        </Script>
       </Head>
+
+      {/* Google Tag Manager */}
+      <Script id="gtm-head" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-P49RJBX');`}
+      </Script>
 
       {/* Google Tag Manager (noscript) */}
       <noscript>
